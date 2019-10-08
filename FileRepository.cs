559d54f35ce398aa38d02cd01ad11b50eeacfa92
Pukste <ville_pukki@hotmail.com>
2019-10-08 07:20:09 +0300

    FileRepository fixed

--- FileRepository.cs
+++ FileRepository.cs
@@ -13,7 +13,6 @@ namespace gamewebapi{
         public Task<Player> Get(Guid id){
             string[] _repository = File.ReadAllLines("game_dev.txt", Encoding.Default);
             foreach(var player in _repository){
-                Console.WriteLine(player);
                 var p =JsonConvert.DeserializeObject<Player>(player);
                 if(p.Id == id){
                     return Task.FromResult(p);
@@ -47,30 +46,37 @@ namespace gamewebapi{
         }
         public Task<Player> Modify(Guid id, ModifiedPlayer player){
 
-            string _repository = File.ReadAllText("game_dev.txt", Encoding.Default);
-            PlayerList players = JsonConvert.DeserializeObject<PlayerList>(_repository);
-            foreach(var oldplayer in players.allPlayers){
-                if(oldplayer.Id == id){
-                    oldplayer.Score = player.Score;
-                    File.WriteAllText("game_dev.txt", JsonConvert.SerializeObject(players));
-                    return Task.FromResult(oldplayer);
+            string[] _repository = File.ReadAllLines("game_dev.txt", Encoding.Default);
+            for(int i = 0; i<_repository.Length; i++){
+                var p =JsonConvert.DeserializeObject<Player>(_repository[i]);
+                if(p.Id == id){
+                    p.Score = player.Score;
+                    _repository[i] = JsonConvert.SerializeObject(p);
+                    File.WriteAllLines("game_dev.txt", _repository);
+                    return Task.FromResult(p);
                 }
             }
             return null;
         }
         public Task<Player> Delete(Guid id){
-
-            string _repository = File.ReadAllText("game_dev.txt", Encoding.Default);
-            PlayerList players = JsonConvert.DeserializeObject<PlayerList>(_repository);
-            for(int i = players.allPlayers.Count()-1; i >= 0; i--){
-                if(players.allPlayers[i].Id == id){
-                    Player deletedPlayer = players.allPlayers[i];
-                    players.allPlayers.RemoveAt(i);
-                    File.WriteAllText("game_dev.txt", JsonConvert.SerializeObject(players));
-                    return Task.FromResult(deletedPlayer);
+            Player deletedPlayer = new Player();
+            List<Player> newlist = new List<Player>();
+            var _repository = File.ReadAllLines("game_dev.txt", Encoding.Default);
+            for(int i = 0; i<_repository.Length; i++){
+                var p = JsonConvert.DeserializeObject<Player>(_repository[i]);
+                if(p.Id != id)
+                    newlist.Add(p);
+                else{
+                    deletedPlayer = p;
                 }
             }
-            return null;
+            string[] newarray = new string[newlist.Count];
+            for(int i=0; i<newlist.Count; i++){
+                newarray[i] = JsonConvert.SerializeObject(newlist[i]);
+            }
+            File.WriteAllLines("game_dev.txt", newarray);
+            return Task.FromResult(deletedPlayer);
+            
         }
     }
 }
