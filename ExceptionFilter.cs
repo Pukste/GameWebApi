3d269f2be716bf44827230fd1233015a732e91c8
Jani Sulo <jani.sulo@metropolia.fi>
2019-10-09 16:46:27 +0300

    Exception filter

---
+++ ExceptionFilter.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace gamewebapi
+{
+    public class ExceptionFilter : ExceptionFilterAttribute {
+        public override void OnException(ExceptionContext context){
+            if (context.Exception is RequirementException){
+                
+                   
+            }
+        }
+    }
+}
